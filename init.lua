84615f50c030e473ada222275acc0c61d1dc4678
Alexandre Gressier <alexandre@gressier.dev>
2025-02-24 15:40:07 +0100

    Customize console appearance

--- init.lua
+++ init.lua
@@ -1,5 +1,11 @@
 hs.alert.show("Spacehammer config loaded")
 
+-- Console appearance
+hs.console.darkMode(true)
+hs.console.consoleFont({ name = "JetBrains Mono Medium", size = 14 })
+hs.console.consolePrintColor({ hex = "#c6c8c6" })
+hs.console.consoleCommandColor({ hex = "#b5bd68" })
+
 -- Support upcoming 5.4 release and also use luarocks' local path
 package.path = package.path .. ";" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?.lua;" .. os.getenv("HOME") .. "/.luarocks/share/lua/5.4/?/init.lua"
 package.cpath = package.cpath .. ";" .. os.getenv("HOME") .. "/.luarocks/lib/lua/5.4/?.so"
